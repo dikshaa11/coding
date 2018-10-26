a6041448f98e3aa1b13e7e2d44f70b106bb771c5
raj10071997 <sahudhanraj123@gmail.com>
2018-10-26 13:21:18 +0530

    add one to number

---
+++ addOneToNumber.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> plusOne(vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    vector<int> ans;
+    int sum =0;
+    for(int i=0;i<A.size();i++)
+    {
+        sum = sum*10 + A[i];
+    }
+    
+    sum = sum+1;
+
+    cout<<sum<<" sum "<<endl;
+    
+    while(sum)
+    {
+        int x = sum%10;
+        ans.push_back(x);
+        sum=sum/10;
+    }
+    
+    for(int i=0;i<(ans.size()/2);i++)
+    {
+        int t = ans[i];
+        ans[i]=ans[ans.size()-1-i];
+        ans[ans.size()-1-i] = t;
+    }
+    
+    return ans;
+}
+
+
+int main()
+{
+     vector<int> a;
+    int x;
+    for(int i=0;i<12;i++)
+    {
+        cin>>x;
+        a.push_back(x);
+    }
+
+    vector<int> A = plusOne(a);
+
+     for(int i=0;i<A.size();i++)
+    cout<<A[i]<<" ";
+}
