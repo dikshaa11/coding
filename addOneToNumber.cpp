52d67a845fd046cccbb9a73204246b388eff3201
Richa <richagautam208@gmail.com>
2019-01-06 11:11:53 +0530

    deleted

--- addOneToNumber.cpp
+++ addOneToNumber.cpp
@@ -3,10 +3,6 @@
 using namespace std;
 
 vector<int> plusOne(vector<int> &A) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
     
     vector<int> ans;
     int sum =0;
